903a1c3cb8857f8e7f63adced8f9f079767a8ab2
Devin Smith <github@arzynik.com>
2015-12-08 09:11:53 -0800

    sample index using tipsy

--- web/index.php
+++ web/index.php
@@ -1,4 +1,18 @@
 <?php
 
-// do nothing right now
-die('done');
+error_reporting(E_ALL ^ (E_NOTICE | E_STRICT));
+ini_set('display_errors',true);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Tipsy\Tipsy;
+
+Tipsy::router()
+	->post('hook', function($Params, $Request) {
+		print_r($Request);
+	})
+	->otherwise(function() {
+		http_response_code(404);
+	});
+
+Tipsy::run();
