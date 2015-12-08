42765283247bab9620cdcd34ac70ef443f43165a
Devin Smith <github@arzynik.com>
2015-12-08 09:49:01 -0800

    progress

---
+++ src/app.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Tipsy\Tipsy;
+
+Tipsy::router()
+	->post('hook', function($Params, $Request) {
+		$package = $_ENV['PACKAGE_NAME'] ? $_ENV['PACKAGE_NAME'] : ($this->tipsy()->config()['update']['package'] ? $this->tipsy()->config()['update']['package'] : $Request->repository->full_name);
+
+		$cmds[] = 'mkdir /tmp/repos';
+		$x = 1;
+		foreach ($_ENV as $k => $v) {
+			if (preg_match('/^GITHUB_REPO[0-9]+$', $k)) {
+				$cmds[] = 'git clone '.$v.' /tmp/repos/'.$x;
+				$cmds[] = 'cd /tmp/repos/'.$x;
+				$cmds[] = 'composer update '.$package.' --optimize-autoloader';
+				$cmds[] = 'git add -A';
+				$cmds[] = 'git commit -m "Automatic dependency update for '.$package.'"';
+			}
+			$x++;
+		}
+
+		foreach ($cmds as $cmd) {
+			echo shell_exec($cmd.' 2>&1')."\n";
+		}
+	})
+	->otherwise(function() {
+		http_response_code(404);
+	});
+
+Tipsy::run();
--- web/index.php
+++ web/index.php
@@ -2,17 +2,4 @@
 
 error_reporting(E_ALL ^ (E_NOTICE | E_STRICT));
 ini_set('display_errors',true);
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use Tipsy\Tipsy;
-
-Tipsy::router()
-	->post('hook', function($Params, $Request) {
-		print_r($Request);
-	})
-	->otherwise(function() {
-		http_response_code(404);
-	});
-
-Tipsy::run();
+require_once __DIR__ . '/../src/app.php';
